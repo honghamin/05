ec78c5723cda5df968475fb534ff8c928c6ce365
honghamin <113421639+honghamin@users.noreply.github.com>
2022-10-04 16:39:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main(int argc, char *argv[]) {
 	int a;
 	printf("정수 하나를 입력하시오 :");
 	scanf("%i", &a);
-	if (a>0 )
-     	printf("양수입니다.");
-	else if (a==0)
-	    printf("0 입니다.");
+	
+	if(a>=0)
+     	printf("절대값은 %d 입니다.",a);
+     	
 	else
-	    printf("음수입니다.") ;
+	    printf("절대값은 %d 입니다.",-1*a) ;
 	return 0;
 }
