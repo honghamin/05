df6b8028834490c046c58ef6c933db52accae551
honghamin <113421639+honghamin@users.noreply.github.com>
2022-10-04 17:57:27 +0900

    05-06

--- main.c
+++ main.c
@@ -4,22 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b;
-	char c;
-	int result;
+	int answer = 59;
+	int x;
+	int trial =0;
 	
-	printf("계산식을 쓰시오:");
-	scanf("%d%c%d", &a,&c,&b);
 	
-	if (c=='+')
-	    result = a+b;
-    else if (c=='-')
-        result=a-b;
-    else if (c=='/')
-        result=a/b;
-    else
-        result=a*b;
-        
-    printf("=%i", result);
+	do
+	{
+		printf("Guess a number: "); //입력해라 문구 출력
+		scanf("%i", &x); //입력을 받음
+		if (x>59)
+		    printf("low!\n");
+		    
+	    else if (x<59)
+		    printf("high!\n");
+		    
+		trial+=1;//입력숫자가 정답보다 큰지 작은지 출력  
+	}
+	while (x!=answer); //정답과 같은지 여부 판단
+	
+	printf("Number of trial = %i\n", trial);
 	return 0;
 }
